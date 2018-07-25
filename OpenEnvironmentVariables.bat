65741b6cfc7fe659e716eac1e0a5521508ebdf7b
Neos21 <neos21@gmail.com>
2018-07-25 20:09:46 +0900

    環境変数を開くバッチ

---
+++ OpenEnvironmentVariables.bat
@@ -0,0 +1,11 @@
+@Echo Off
+
+Rem 「環境変数」ウィンドウを開く
+
+
+
+Start C:\Windows\system32\rundll32.exe sysdm.cpl, EditEnvironmentVariables
+
+
+
+Rem ・Corredor - 「環境変数」ウィンドウを直接開くバッチファイル : http://neos21.hatenablog.com/entry/2017/01/26/%E3%80%8C%E7%92%B0%E5%A2%83%E5%A4%89%E6%95%B0%E3%80%8D%E3%82%A6%E3%82%A3%E3%83%B3%E3%83%89%E3%82%A6%E3%82%92%E7%9B%B4%E6%8E%A5%E9%96%8B%E3%81%8F%E3%83%90%E3%83%83%E3%83%81%E3%83%95%E3%82%A1%E3%82%A4
