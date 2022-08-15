ff1414f256646d545221935ac251e4641cc960c7
Neos21 <neos21@gmail.com>
2022-08-16 01:01:27 +0900

    Update

--- windows-batch/smells-like-cmd.bat
+++ windows-batch/smells-like-cmd.bat
@@ -1,7 +1,6 @@
 @Echo Off
 
-Rem コマンドプロンプトは制限されているが、バッチファイルなら動作する環境で、
-Rem バッチファイルでコマンドプロンプトを再現する。
+Rem コマンドプロンプトが制限されているがバッチファイルなら動作する、という環境で、バッチファイルでコマンドプロンプトを再現する。
 Rem 
 Rem 「コマンドプロンプトは管理者によって使用不可にされています」という制限は、以下のレジストリで制限されている。
 Rem User Key   : [HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\System]
@@ -9,10 +8,23 @@ Rem Value Name : DisableCMD
 Rem Data Type  : REG_DWORD (DWORD Value)
 Rem Value Data : 0 = default, 1 = disabled, 2 = disabled but allow batch
 
-
-
 Rem このバッチファイルがあるフォルダをカレントフォルダにする (UNC パス上でも OK)
 Pushd %0\..
+
+Rem 簡易エイリアス設定
+Doskey alias=if "$1"=="" (doskey /macros) else (doskey $*)
+Doskey cat=type $*
+Doskey cp=copy $*
+Doskey h=doskey /history
+Doskey history=doskey /history
+Doskey ll=dir /ad
+Doskey ls=dir /w
+Doskey lv=more /E $*
+Doskey mv=move $*
+Doskey pwd=echo %CD%
+Doskey quit=exit
+Doskey tree=tree /f $b more
+Doskey unalias=doskey $*=
 
 Rem Exit するまで無限ループ
 :LOOP
@@ -22,17 +34,15 @@ Set /p CMD_EVAL=%CD%^>
 
 Rem 未入力なら処理しない
 If "%CMD_EVAL%" == "" Goto :LOOP
-
+ 
 Rem 受け取った文字列をそのままコマンドとして実行する
 %CMD_EVAL%
 
-Rem 空行開けて入力値を初期化する
+Rem 空行を開けて入力値を初期化する
 Echo.
 Set CMD_EVAL=
 
 Goto :LOOP
-
-
 
 Rem 参考
 Rem ・http://www.pctools.com/guides/registry/detail/1143/
